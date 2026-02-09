9b71cbe5200e5bacdc80b6b0f335906495a0e7c3
Guilherme <jg21504956@gmail.com>
2026-02-09 09:02:05 -0300

    chore: start the upgrade admin process

--- src/api/adapters/admin/routes.py
+++ src/api/adapters/admin/routes.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from sqladmin import Admin, ModelView
-from sqladmin.filters import AllUniqueStringValuesFilter
+from sqladmin.filters import AllUniqueStringValuesFilter, OperationColumnFilter
 
 from api.adapters.factories import FoodFactory
 from api.adapters.mapping import FoodMapping
-from api.adapters.schemas.models import CategoryModel, FoodModel
+from api.adapters.schemas.models import CategoryModel, FoodModel, FoodPhotoModel
 from api.infra.db import engine
 
 
@@ -12,13 +12,19 @@ def register_admin(app : FastAPI):
     admin = Admin(app , engine)
 
     class FoodAdmin(ModelView , model = FoodModel): #type: ignore
-        column_list = [FoodModel.id , FoodModel.name , FoodModel.slug , FoodModel.price]
+        column_list = [FoodModel.id , FoodModel.name , FoodModel.price]
         column_filters = [
-            AllUniqueStringValuesFilter(FoodModel.name)
+            OperationColumnFilter(FoodModel.price)
         ]
-        column_details_list = [*column_list]
+        column_details_list = [*column_list , FoodModel.slug ]
         form_columns = ["name" , "description" , "price" , "photos"]
-
+        category = "Meal"
+        name = "Food"
+        name_plural = "Foods"
+        icon = "fa-solid fa-bowl-food"
+        category_icon = "fa-solid fa-bowl-food"
+        column_searchable_list = [FoodModel.name]
+        column_default_sort = [(FoodModel.name , False)]
 
         async def on_model_change(self, data, model, is_created, request):
             factory = FoodFactory(ignore_attrs=["id"])
@@ -38,6 +44,23 @@ def register_admin(app : FastAPI):
         column_filters = [
             AllUniqueStringValuesFilter(CategoryModel.name)
         ]
+        category = "Meal"
+        name = "Category"
+        name_plural ="Categories"
+        column_searchable_list = [CategoryModel.name]
+        column_default_sort = [(CategoryModel.name , False)]
+
+
+    class FoodPhotoAdmin(ModelView , model=FoodPhotoModel): #type: ignore
+        column_list = [FoodPhotoModel.id , FoodPhotoModel.food]
+        name = "Food Photo"
+        name_plural = "Foods Photos"
+        icon = "fa-solid fa-image"
+        category = "Meal"
+        column_formatters = {FoodPhotoModel.food : lambda model , column : model.food.name }
+        column_formatters_detail = {FoodPhotoModel.food : lambda model , column : model.food.name }
+        column_details_exclude_list = [FoodPhotoModel.food_id]
 
     admin.add_view(FoodAdmin)
+    admin.add_view(FoodPhotoAdmin)
     admin.add_view(CategoriesAdmin)
