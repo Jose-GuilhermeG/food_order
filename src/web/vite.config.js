d46a521275ac9275d25402b6a757de3406c00e7a
Guilherme <jg21504956@gmail.com>
2026-02-13 12:23:46 -0300

    feat: add dockercompose

--- src/web/vite.config.js
+++ src/web/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import { qrcode } from 'vite-plugin-qrcode'
 
 export default defineConfig({
+  root : "./src/web",
   plugins: [
     react(),
     tailwindcss(),
