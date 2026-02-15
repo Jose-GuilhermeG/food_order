e2f63d0fb815d29a6c4aa9a50a7396987dc2513d
Guilherme <jg21504956@gmail.com>
2026-02-15 13:59:39 -0300

    feat: upgrade models

--- src/api/adapters/schemas/models.py
+++ src/api/adapters/schemas/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Text
+from sqlalchemy import Column, Numeric, Text
 from sqlmodel import Field, Relationship, SQLModel
 
 
@@ -11,18 +11,40 @@ class FoodCategoryModel(
     BaseModel,
     table = True #type: ignore[call-arg]
 ):
-    food_id : int = Field(foreign_key="foodmodel.id")
-    category_id : int = Field(foreign_key="categorymodel.id")
+    food_id : int = Field(foreign_key="food.id")
+    category_id : int = Field(foreign_key="category.id")
 
 
 class FoodModel(
     BaseModel,
     table = True #type: ignore[call-arg]
 ):
-    name : str
-    slug : str
-    description : str
-    price : float
+    __tablename__ = "food"
+
+    name : str = Field(
+        title="Nome do alimento",
+        max_length=120 ,
+        index=True
+    )
+    slug : str = Field(
+        title="slug do alimento",
+        max_length=120 ,
+        unique=True
+    )
+    description : str | None = Field(
+        title="Descrição do alimento",
+        default="" ,
+        max_length=255
+    )
+    price : float = Field(
+        title="Preço do alimento",
+        max_digits=10,
+        decimal_places=2,
+        sa_column=Column(
+            Numeric(10,2)
+        ),
+        ge=1
+    )
     categories : list["CategoryModel"] = Relationship(
         back_populates="foods",
         link_model=FoodCategoryModel
@@ -35,9 +57,23 @@ class CategoryModel(
     BaseModel,
     table = True #type: ignore[call-arg]
 ):
-    name : str = Field(unique=True)
-    image : str = Field(sa_column=Column(Text))
-    slug : str = Field(unique = True)
+    __tablename__ = "category"
+
+    name : str = Field(
+        title="Nome da categoria",
+        max_length=90,
+        unique=True
+    )
+    image : str = Field(
+        title="Imagem da categoria",
+        default="",
+        sa_column=Column(Text)
+    )
+    slug : str = Field(
+        title="Slug da categoria",
+        max_length=90,
+        unique = True
+    )
 
     foods : list["FoodModel"] = Relationship(
         back_populates="categories",
@@ -48,6 +84,9 @@ class FoodPhotoModel(
     BaseModel,
     table = True #type: ignore[call-arg]
 ):
-    food_id : int = Field(foreign_key="foodmodel.id")
-    photo_url : str
+    food_id : int = Field(foreign_key="food.id")
+    photo_url : str = Field(
+        title="Url da foto ddo alimento",
+        sa_column=Column(Text)
+    )
     food: "FoodModel" = Relationship(back_populates="photos")
